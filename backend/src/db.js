5c67b6c6cc7d01e84a0bc86ffe0d50ce13f53c39
Jad-Kreit29 <jad.kreitjob@gmail.com>
2026-04-06 15:07:25 -0400

    Updated database

---
+++ backend/src/db.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const Database = require('better-sqlite3');
+
+const dbPath = path.join(__dirname, 'hotel-database.db');
+const db = new Database(dbPath);
+
+// Good default for relational schemas
+db.pragma('foreign_keys = ON');
+
+module.exports = db;
